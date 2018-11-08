1698ba82d7d8fa40d6f18a215234fdee076925e1
joyote <44850270+joyote@users.noreply.github.com>
2018-11-07 22:53:42 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Markup
 import blockclock
 app = Flask(__name__)
-app.secret_key = 'RHPTjL6Kg45CuE3Ld7vKmDpC'
+app.secret_key = 'super_secret_key_here'
 
 @app.route('/', methods=["GET", "POST"])
 def homepage():
@@ -113,4 +113,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
